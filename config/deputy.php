644b30dab4fa7406086dc854264ab777b504a20c
Blitheness <blitheness@oneiric.co.uk>
2018-07-09 15:57:47 +0100

    Update config syntax

--- config/deputy.php
+++ config/deputy.php
@@ -20,9 +20,9 @@ return [
     // Should all employees be a part of the company defined in 'default_company' ?
     // This will add all newly created or activated employees to company 'default_company'
     // Do not set this to true if default_company is set to null or 0
-    'all_in_default_company' = false,
+    'all_in_default_company' => false,
 
     // Should an employee be terminated if its last remaining company is 'default_company' ?
     // Do not set this to true if default_company is set to null or 0
-    'terminate_if_only_in_default' = false
+    'terminate_if_only_in_default' => false
 ];
